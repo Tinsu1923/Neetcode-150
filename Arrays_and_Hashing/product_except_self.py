12d1404b53e3b9a74dfa532d1cc97b72edb0a886
tinsu1923 <tindaniel1923@gmail.com>
2025-07-07 08:49:59 -0400

    A more efficient solution in terms of space and time, even if it's still linear.

--- Arrays_and_Hashing/product_except_self.py
+++ Arrays_and_Hashing/product_except_self.py
@@ -19,5 +19,20 @@ class Solution:
        
         return res
             
+    def productExceptSelf2(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        res = [0] * n
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix = prefix * nums[i]
+        
+        postfix = 1
+        for j in range(len(nums) - 1, -1, -1):
+            res[j] = res[j] * postfix
+            postfix = postfix * nums[j]
+        
+        return res
 
         
